9cce99bbe779191be2172e41ff817ecaac828938
Chris Heppell <chris.heppell@myswimmingclub.uk>
2024-01-11 14:02:18 +0000

    Bug fix in parameters

--- src/task.parameters.ts
+++ src/task.parameters.ts
@@ -14,7 +14,7 @@ export class TaskParameters {
         this._azurePipelineName = core.getInput('azure-pipeline-name', { required: true });
         this._azureDevopsToken = core.getInput('azure-devops-token', { required: true });
         this._azurePipelineVariables = core.getInput('azure-pipeline-variables', { required: false });
-        this._azurePipelineSourceBranch = core.getInput('azure-pipeline-source-branch', { required: false });
+        this._azurePipelineSourceBranch = core.getInput('azure-devops-source-branch', { required: false });
 
         let parameters = null;
         try {
